a1b016253f4336d884106c8419857e0a0ed9e7ff
nkondengineer <104311349+nkondengineer@users.noreply.github.com>
2022-05-14 11:21:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ window.addEventListener("scroll", () => {
   }
 });
 
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+};
+function off() {
+  document.getElementById("overlay").style.display = "none";
+};
+
 © 2022 GitHub, Inc.
 Terms
 Privacy
